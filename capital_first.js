a66a8e287fe5fa34774eec9bc7a28a537f05d608
Rankireddy <akarsh.1@iitj.ac.in>
2024-08-24 10:25:14 +0530

    new program

--- capital_first.js
+++ capital_first.js
@@ -2,6 +2,7 @@
 //Write a function to capitalize the first letter of each word in a sentence.
 
 function capitalizeWords(str){
-    return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() +word.slice(1)).join(' ')
+   return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() +word.slice(1)).join(' ');
 }
+
 console.log(capitalizeWords("hello World iam mouni"));
